fb92c656d5bda2256f5d2b662036486a30f8f5b7
brandontylerhall <bran.tyler.hall@gmail.com>
2026-04-15 15:45:49 -0400

    pausing for today

--- src/main/java/com/example/LootLoggerPlugin.java
+++ src/main/java/com/example/LootLoggerPlugin.java
@@ -4,10 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.NPC;
+import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.events.AnimationChanged;
+import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
@@ -16,6 +16,7 @@ import net.runelite.client.events.NpcLootReceived;
 import net.runelite.client.plugins.loottracker.LootReceived;
 
 import javax.inject.Inject;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -35,6 +36,7 @@ public class LootLoggerPlugin extends Plugin {
     private final Gson prettyGson = new GsonBuilder().setPrettyPrinting().create(); // For your debug chat/logs
 
     private java.io.FileWriter writer;
+    private int lastActiveAnimation = -1;
 
     // =========================
     //    START UP PROCEDURE
@@ -94,6 +96,51 @@ public class LootLoggerPlugin extends Plugin {
 
         LootRecord record = new LootRecord(sourceName, xCoord, yCoord, planeCoord, items);
         executor.execute(() -> writeToFile(record));
+    }
+
+    // =========================
+    //    ANIMATION CHECKER
+    // =========================
+    @Subscribe
+    public void onAnimationChanged(AnimationChanged event) {
+        if (event.getActor() != client.getLocalPlayer()) return;
+
+        int anim = client.getLocalPlayer().getAnimation();
+        if (anim != -1) {
+            lastActiveAnimation = anim;
+        }
+    }
+
+    // =========================
+    //    RESOURCE NODE EVENT
+    // =========================
+    @Subscribe
+    public void onItemContainerChanged(ItemContainerChanged event) {
+        // 93 is the ID for the Inventory container
+        if (event.getContainerId() == 93) {
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Inventory changed! Checking for animation...", null);
+
+            int animationId = client.getLocalPlayer().getAnimation();
+
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Animation ID: " + animationId, null);
+
+            String items = Arrays.toString(event.getItemContainer().getItems());
+
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Items: " + items, null);
+
+        }
+    }
+
+    // =========================
+    //        IDK YET LOL
+    // =========================
+    @Subscribe
+    public void onLootReceived(LootReceived event) {
+        String message = String.format("Loot: %s | Stacks: %d | Type: %s",
+                event.getName(), event.getItems().size(), event.getType());
+
+        client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", message, null);
+        log.info(message);
     }
 
     // =========================
