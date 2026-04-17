276163fa64939633b871107a511f3e45cf16ed1f
brandontylerhall <bran.tyler.hall@gmail.com>
2026-04-17 16:26:14 -0400

    attempting to move giant for loop from LootLoggerPlugin to here to clean up code.

---
+++ src/main/java/com/lootlogger/util/InventoryProcessor.java
@@ -0,0 +1,13 @@
+package com.lootlogger.util;
+
+import net.runelite.api.Item;
+
+public class InventoryProcessor {
+    public void invProcess(
+            Item[] previousInventory,
+            Item[] currentInventory,
+            boolean isBanking,
+            String lastMenuOptionClicked) {
+
+    }
+}
