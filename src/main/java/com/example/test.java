b835ea13c0d04afde6af899fb3930a5394dc9461
brandontylerhall <bran.tyler.hall@gmail.com>
2026-04-17 07:34:16 -0400

    pause

---
+++ src/main/java/com/example/test.java
@@ -0,0 +1,14 @@
+//package com.example;
+//
+//public class test {
+//    if (newId != -1 && (newId != oldId || newQty > oldQty)) {
+//        int amount = (newId == oldId) ? (newQty - oldQty) : newQty;
+//
+//        // Only log to file if we aren't banking
+//        if (!isBanking) {
+//            handleGatheringGains(newId, amount);
+//        } else {
+//            gameMsg("Withdrawal detected, skipping log...");
+//        }
+//    }
+//}
