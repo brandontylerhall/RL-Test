fd6be2c305be696e5437c2e20f9defa5977d45c7
Brandon Hall <bran.tyler.hall@gmail.com>
2026-04-19 00:30:58 -0400

    created enum ActionType to be able to quickly assign functionality based on action

---
+++ src/main/java/com/lootlogger/data/ActionType.java
@@ -0,0 +1,11 @@
+package com.lootlogger.data;
+
+public enum ActionType {
+    DESTROY, // will cover drop, break, bury, etc.
+    CONSUME, // will cover use, eat, drink, etc.
+    GATHER_GAIN, // will cover all types of gathering gains
+    BANK_DEPOSIT,
+    BANK_WITHDRAWAL,
+    SWAP,
+    TAKE
+}
