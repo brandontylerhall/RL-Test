a16b68f2fcc2339e50ad974efbd74d3f54b7cc5b
brandontylerhall <bran.tyler.hall@gmail.com>
2026-04-21 15:23:37 -0400

    added name field

--- src/main/java/com/lootlogger/data/DroppedItem.java
+++ src/main/java/com/lootlogger/data/DroppedItem.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 @Getter
 public class DroppedItem {
     private final int id;
+    private final String name;
     private final int qty;
     private final int GE;
     private final int HA;
 
-    public DroppedItem(int id, int qty, int GE, int HA) {
+    public DroppedItem(int id, String name, int qty, int GE, int HA) {
         this.id = id;
+        this.name = name;
         this.qty = qty;
         this.GE = GE;
         this.HA = HA;
