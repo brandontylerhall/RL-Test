319dbe4fe69658eecf862ef85cc8b9bfa112f3cf
brandontylerhall <bran.tyler.hall@gmail.com>
2026-04-22 12:41:16 -0400

    added more cases

--- src/main/java/com/lootlogger/data/ActionType.java
+++ src/main/java/com/lootlogger/data/ActionType.java
@@ -1,12 +1,15 @@
 package com.lootlogger.data;
 
 public enum ActionType {
-    DESTROY, // will cover drop, break, bury, etc.
-    CONSUME, // will cover use, eat, drink, etc.
-    GATHER_GAIN, // will cover all types of gathering gains
+    DESTROY,
+    CONSUME,
+    GATHER_GAIN,
     BANK_DEPOSIT,
     BANK_WITHDRAWAL,
     SWAP,
     TAKE,
-    DROP
+    DROP,
+    RANGED_FIRE,
+    SPELL_CAST,
+    COMBAT_CONSUME
 }
