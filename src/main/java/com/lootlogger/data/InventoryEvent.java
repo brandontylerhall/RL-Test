8ff2c93ff3be5a3389f984807af88a3775773a0f
brandontylerhall <bran.tyler.hall@gmail.com>
2026-04-22 12:41:41 -0400

    added targetName to track ranged attacks

--- src/main/java/com/lootlogger/data/InventoryEvent.java
+++ src/main/java/com/lootlogger/data/InventoryEvent.java
@@ -1,14 +1,15 @@
 package com.lootlogger.data;
 
 public class InventoryEvent {
+    public String targetName;
     public ActionType actionType;
     public int itemId;
     public int qty;
 
-
-    public InventoryEvent(ActionType actionType, int itemId, int qty) {
-        this.actionType = actionType;
+    public InventoryEvent(int itemId, int qty, ActionType actionType, String targetName) {
         this.itemId = itemId;
         this.qty = qty;
+        this.actionType = actionType;
+        this.targetName = targetName;
     }
 }
